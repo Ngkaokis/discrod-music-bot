b2802c51d890162ce667bfdb56136fe813724232
fei <tpf1231000@gmail.com>
2023-07-06 19:53:50 +0800

    add youtube dlp func

--- handler/musicPlayerHandler.go
+++ handler/musicPlayerHandler.go
@@ -3,57 +3,81 @@ package handler
 import (
 	"discord-bot/util"
 	"fmt"
+	"log"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 
-func MusicPlayerHandler(s *discordgo.Session, m *discordgo.MessageCreate  ) {
-
+func MusicPlayerHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	
+	
 	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
  
-fmt.Println(s.VoiceConnections)
-	if m.Content == "Play" {
-		
-		fmt.Println("working")
-	
-		//find the voice channel of user
-		voiceChannel,err := util.SearchVoiceChannel(s , m)
-		if err != nil {
-			s.ChannelMessageSend(m.ChannelID, "OwO Please join the voice channel OwO")
-			fmt.Println("Can't find the voice channel",err)
-			return
-		}
-		fmt.Println("This is voice Channel:",voiceChannel )
-		
-		
-		
-		vc ,err :=s.ChannelVoiceJoin(m.GuildID,voiceChannel,false,true)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		
-		
-		// if err != nil {
-		// 	time.Sleep(2 * time.Second)
-		// 	vc, err = s.ChannelVoiceJoin("YOUR_GUILD_ID", "", false, false)
-		// 	if err != nil {
-		// 		panic(err)
-		// 	}
-		// }
+	command, query, hasPrefix := util.ParsePrefix(m.Content)
 
-	vc.Close()
-	// defer vc.Disconnect()
+	if !hasPrefix {
+		return
 	}
 
+	switch command {
+		case "youtube" , "yt":
+			fmt.Println(m.Content)
+			fmt.Println(query)
+			
+		case "skip":
+			//TODO SKIP implementation
+		
+		case "stop":
+			//TODO STOP
+		
+		
+		default: break;
+	}
+
+	// if m.Content == "Play" {
+		
+	// 	fmt.Println("working")
+	
+	// 	//find the voice channel of user
+	// 	voiceChannel,err := util.SearchVoiceChannel(s , m)
+	// 	if err != nil {
+	// 		s.ChannelMessageSend(m.ChannelID, "OwO Please join the voice channel OwO")
+	// 		fmt.Println("Can't find the voice channel",err)
+	// 		return
+	// 	}
+	// 	fmt.Println("This is voice Channel:",voiceChannel )
+		
+		
+		
+	// 	vc ,err :=s.ChannelVoiceJoin(m.GuildID,voiceChannel,false,true)
+	// 	if err != nil {
+	// 		fmt.Println("Can't join the channel:",err)
+	// 		return
+	// 	}
+		
+		
+	// 	dgvoice.PlayAudioFile(vc,"music.mp3",make(chan bool))
+	// 	defer  vc.Disconnect()
+	// }
+
+	
 	
 }
+
+func handleYoutubeCommand(s *discordgo.Session, m *discordgo.MessageCreate, query string) {
+	voiceChannel,err := util.SearchVoiceChannel(s , m);
+	if err!=nil{
+		log.Printf("Can't find the voice channel",err);
+	}
+	fmt.Println(voiceChannel)
+	//TODO
+	
+	
+	}
 
 
