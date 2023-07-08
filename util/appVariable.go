8e9b0726b7728142a0dae41da1ef2fb90dc5e0d2
fei <tpf1231000@gmail.com>
2023-07-08 20:29:54 +0800

    global variable for Guilds and YoutubeService

---
+++ util/appVariable.go
@@ -0,0 +1,17 @@
+package util
+
+import (
+	"discord-bot/services"
+	"discord-bot/types"
+	"sync"
+)
+
+var  (
+
+	Guilds 		= make(map[string]*types.ActiveGuild)
+	GuildsMutex = sync.RWMutex{}
+	GuildNames   = make(map[string]string)
+
+	YoutubeService *services.Service
+
+)
