485b4252165d4b0ad7cf963c1fcb44546b348ce8
fei <tpf1231000@gmail.com>
2023-07-16 16:45:22 +0800

    complete handleYoutubeCommand function

--- handler/musicPlayerHandler.go
+++ handler/musicPlayerHandler.go
@@ -4,66 +4,139 @@ import (
 	"discord-bot/types"
 	"discord-bot/util"
 	"discord-bot/workers"
+	"fmt"
 	"os"
 
-	"fmt"
+	"log"
 
+	"github.com/bwmarrin/dgvoice"
 	"github.com/bwmarrin/discordgo"
 )
 
 
-func MusicPlayerHandler(s *discordgo.Session, m *discordgo.MessageCreate  ) {
+func MusicPlayerHandler(bot *discordgo.Session, message *discordgo.MessageCreate) {
 
 	
-	
-	// Ignore all messages created by the bot itself
-	if message.Author.ID == bot.State.User.ID {
-		return
-	}
- 
-fmt.Println(s.VoiceConnections)
-	if m.Content == "Play" {
+	if message.Content == "Play" {
 		
 		fmt.Println("working")
 	
 		//find the voice channel of user
-		voiceChannel,err := util.SearchVoiceChannel(s , m)
+		voiceChannel,err := util.SearchVoiceChannel(bot , message)
 		if err != nil {
-			s.ChannelMessageSend(m.ChannelID, "OwO Please join the voice channel OwO")
+			bot.ChannelMessageSend(message.ChannelID, "OwO Please join the voice channel OwO")
 			fmt.Println("Can't find the voice channel",err)
 			return
 		}
+			
 		fmt.Println("This is voice Channel:",voiceChannel )
 		
 		
 		
-		vc ,err :=s.ChannelVoiceJoin(m.GuildID,voiceChannel,false,true)
+		vc ,err :=bot.ChannelVoiceJoin(message.GuildID,voiceChannel,false,true)
 		if err != nil {
 			fmt.Println("Can't join the channel:",err)
 			return
 		}
 		
 		
-		// if err != nil {
-		// 	time.Sleep(2 * time.Second)
-		// 	vc, err = s.ChannelVoiceJoin("YOUR_GUILD_ID", "953675829668347964", false, false)
-		// 	if err != nil {
-		// 		panic(err)
-		// 	}
-		// }
-		
-		dgvoice.PlayAudioFile(vc,"music.mp3",make(chan bool))
+		dgvoice.PlayAudioFile(vc,"data/1689458200-57NboSm4CJU.opus.mp3",make(chan bool))
 		defer  vc.Disconnect()
 	}
-
 	
-
-	
-}
-
-
-
-		// Append encoded pcm data to the buffer.
-		buffer = append(buffer, InBuf)
+	// Ignore all messages created by the bot itself
+	if message.Author.ID == bot.State.User.ID {
+		return
 	}
+ 
+	command, query, hasPrefix := util.ParsePrefix(message.Content)
+	if !hasPrefix {
+		return
+	} 
+		
+	util.GuildsMutex.Lock()
+	activeGuild := util.Guilds[message.GuildID]
+	util.GuildsMutex.Unlock()
+	
+	switch command {
+		case "youtube" , "yt":
+			handleYoutubeCommand(bot, message, activeGuild, query)
+			
+		case "skip":
+			//TODO SKIP implementation
+		
+		case "stop":
+			//TODO STOP
+		
+		
+		default: break;
+	}
+
+
+	
+	
 }
+
+func handleYoutubeCommand(bot *discordgo.Session,message *discordgo.MessageCreate,
+	activeGuild *types.ActiveGuild,  query string) {
+	
+	//* handle the queue is full
+	if activeGuild != nil{
+		if activeGuild.IsMediaQueueFull(){
+			_, _ = bot.ChannelMessageSend(message.ChannelID, "The queue is full!")
+			return
+		}
+	} else {
+		//* If first time to play music, add the guild
+		activeGuild = types.NewActiveGuild(util.GetGuildNameByID(bot, message.GuildID))
+		util.GuildsMutex.Lock()
+		util.Guilds[message.GuildID] = activeGuild
+		util.GuildsMutex.Unlock()
+	}
+	
+	voiceChannelId,err := util.SearchVoiceChannel(bot , message);
+	if err!=nil{
+		log.Printf("[%s] Can't find the voice channel",err);
+		bot.ChannelMessageSend(message.ChannelID, err.Error())
+		return
+	} else {
+		log.Printf("Success");
+	}
+	log.Println(voiceChannelId)
+
+	log.Printf("[%s] Searching for \"%s\"", activeGuild.Name, query)
+
+	//* Search music
+	media, err := util.YoutubeService.SearchYTAndDownload(query)
+	if err != nil {
+		log.Printf("[%s] Unable to find video for query \"%s\": %s", activeGuild.Name, query, err.Error())
+		return
+	}
+
+	createNewWorker := false
+	if !activeGuild.IsStreaming() {
+		log.Printf("[%s] Preparing for streaming", activeGuild.Name)
+		activeGuild.PrepareForStreaming(20)
+		// If the channel was nil, it means that there was no worker
+		createNewWorker = true
+	}
+
+	//* Add song to guild queue
+	activeGuild.EnqueueMedia(media)
+
+	if createNewWorker {
+		log.Printf("[%s] Starting worker", activeGuild.Name)
+		go func() {
+			err = workers.Worker(bot, activeGuild, message.GuildID, voiceChannelId)
+			if err != nil {
+				log.Printf("[%s] Failed to start worker: %s", activeGuild.Name, err.Error())
+				_, _ = bot.ChannelMessageSend(message.ChannelID, fmt.Sprintf("❌ Unable to start voice worker: %s", err.Error()))
+				_ = os.Remove(media.FilePath)
+				return
+			}
+		}()
+	}
+
+	}
+
+
